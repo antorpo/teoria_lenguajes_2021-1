9375ed42ac4947600acaf0cbc6390e37971cdb30
Antonio Gonzalez <antonio.gonzalezr@udea.edu.co>
2021-05-04 12:26:07 -0500

    Despliegue heroku

--- practica_01/settings.py
+++ practica_01/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
